f88857d17d9604551aef9d80a9576d1832bd31a0
wooneusean <20278298+wooneusean@users.noreply.github.com>
2023-02-23 23:29:21 +0800

    Restructure code and rethinking life

--- src/main/java/my/edu/apu/shared/Constants.java
+++ src/main/java/my/edu/apu/shared/Constants.java
@@ -1,19 +1,4 @@
 package my.edu.apu.shared;
 
 public class Constants {
-    public static final String FLIGHT_CONTROLLER_ROUTING_KEY = "flight-controller";
-    public static final String ALTITUDE_CONSUMER_TOPIC = "sensor.altitude";
-    public static final String CABIN_CONSUMER_TOPIC = "sensor.cabin";
-    public static final String SENSORY_TO_CONTROLLER_EXCHANGE_NAME = "sensory-to-controller";
-    public static final String AIRPLANE_STATE_UPDATER_EXCHANGE_NAME = "airplane-state-exchange";
-    public static final String AIRPLANE_STATE_UPDATE_ROUTING_KEY = "airplane-state";
-    public static final String ENGINE_THROTTLE = "engineThrottle";
-    public static final String WING_ANGLE = "wingAngle";
-    public static final String TAIL_ANGLE = "tailAngle";
-    public static final String LANDING_GEAR_DEPLOYED = "landingGearDeployed";
-    public static final String OXYGEN_MASKS_DEPLOYED = "oxygenMasksDeployed";
-    public static final String ALL_SENSORS_TOPIC = "sensor.*";
-    public static final String CONTROLLER_TO_ACTUATOR_EXCHANGE_NAME = "controller-to-actuator";
-    public static final String ENGINE_ACTUATOR_ROUTING_KEY = "actuator.engine";
-    public static final String SPEED_CONSUMER_TOPIC = "sensor.speed";
 }
